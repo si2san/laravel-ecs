d6092090636598711c82c182e8cb1025db23b6bd
sithusan <sithusan@hivelocity.co.jp>
2024-10-11 12:55:24 +0700

    update nginx

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/health-check', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'Health check passed',
+    ]);
+});
